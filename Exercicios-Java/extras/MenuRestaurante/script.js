58674a05d1c82cdc075c1e408d480723199f0e1c
João Elias Maccari <joaomaccari@hotmail.com>
2022-04-28 22:34:19 -0300

    att

---
+++ Exercicios-Java/extras/MenuRestaurante/script.js
@@ -0,0 +1,20 @@
+var submit = document.getElementById('enviar');
+
+function calculaTotal () {
+    //selecionando as opcoes
+    var select = document.getElementById('sabores');
+    var saborFrango = document.getElementById('op-frango') = "frango";
+    var saborCalabresa = document.getElementById('op-calabresa') = "calabresa";
+    var saborStrogonoff = document.getElementById('op-strogonoff')= "strogonoff";
+    var saborChocolota = document.getElementById('op-chocolate') = "chocolate";
+
+   
+
+    switch (select) {
+        case 1: ""
+    }
+
+    var submit = document.getElementById('submit'); 
+}
+
+submit.addEventListener('click', calculaTotal)
