b9a9b870af7c8565393d58df5e2a96ae59c9fa20
João Elias Maccari <joaomaccari@hotmail.com>
2022-05-24 15:49:18 -0300

    att

--- reservaCinema/js/script.js
+++ reservaCinema/js/script.js
@@ -1,32 +1,61 @@
-var freeSeat = ["A1", "A2", "A3" , "A4", "A5", "A6", "A7", "A8", "A9"];
+var freeSeat = ["A1", "A2", "A3" , "A4", "A5", "A6", "A7", "A8", "A9", "A10"];
 var busySeat = []
+var booking = document.getElementsByClassName("booking")[0];
+
 
 function select(seat) {
+
+    
     
     console.log(freeSeat);
-
+    
+    console.log(seat.value)
     for (i in freeSeat) {
+        
         if (freeSeat[i] == seat.value) {
             busySeat.push(seat.value);
             freeSeat.splice(i, 1);
             
             console.log(busySeat);
-            console.log(freeSeat)
+            console.log(freeSeat);
+            var btn = document.getElementsByClassName[i]
+            console.log(btn);
         }
-
+      
     }
+    
+    booking.innerHTML = `cadeira ${seat.value} reservada!`
 
-    isBusy(seat.value)
+    // newbooking(seat);
 
-}
-
-
-function isBusy(bSeat) {
-     if (busySeat.indexOf(bSeat.value) !== -1) {
-         alert("ocupado")
-     }
+    
 
 }
+
+// function newbooking(seat) {
+//     let cadeira = seat.value
+    
+
+//     let flag = true;
+
+//     while (flag == true) {
+//         let newb = prompt("Deseja fazer uma nova reserva?");
+        
+//         if (newb == "s") {
+//             if (busySeat.indexOf(cadeira) != -1) {
+//                 alert("ocupado")
+                
+//             }else{
+
+//             }
+//         }
+
+        
+        
+//     }
+
+// }
+
 
     
     
