16ad958c89739922cc96e87c3ecab7ad689fde76
João Elias Maccari <joaomaccari@hotmail.com>
2022-05-24 20:33:15 -0300

    Update script.js

--- reservaCinema/js/script.js
+++ reservaCinema/js/script.js
@@ -1,35 +1,39 @@
 var freeSeat = ["A1", "A2", "A3" , "A4", "A5", "A6", "A7", "A8", "A9", "A10"];
 var busySeat = []
 var booking = document.getElementsByClassName("booking")[0];
-
+  let click =0
 
 function select(seat) {
-
+    click++
+    let allFree = true
     
-    
-    console.log(freeSeat);
-    
-    console.log(seat.value)
-    for (i in freeSeat) {
+    if (click < 1) {
         
-        if (freeSeat[i] == seat.value) {
-            busySeat.push(seat.value);
-            freeSeat.splice(i, 1);
+            for (i in freeSeat) {
+        
+                if (freeSeat[i] == seat.value) {
+                 busySeat.push(seat.value);
+                freeSeat.splice(i, 1);
             
-            console.log(busySeat);
-            console.log(freeSeat);
-            var btn = document.getElementsByClassName[i]
-            console.log(btn);
+                console.log(busySeat);
+                console.log(freeSeat);
+            }else if (seat.value == freeSeat[i]) {
+
         }
       
     }
+
+    }
+    
+    
+
+    
+    
+    
     
     booking.innerHTML = `cadeira ${seat.value} reservada!`
 
     // newbooking(seat);
-
-    
-
 }
 
 // function newbooking(seat) {
