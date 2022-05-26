a46007a8b98fbb160d7edeecc4e2a71d8793134b
João Elias Maccari <joaomaccari@hotmail.com>
2022-05-25 23:29:56 -0300

    js pronto

--- reservaCinema/js/script.js
+++ reservaCinema/js/script.js
@@ -3,65 +3,51 @@ var busySeat = []
 
 
 
-function select(seat) {
-            var booking = document.getElementsByClassName("booking")[0];
-            for (i in freeSeat) {
-                freeSeat[i] = true
-            }
-
-            disponiveis(seat)
-
-            for (i in freeSeat) {
+function select(s) {
+    let seat = s.value
+    console.log(s)
+    let booking = document.getElementsByClassName("booking")[0];
+    
+        if (freeSeat.indexOf(seat) != -1 ) {
+            
+            ocupar(seat)
+            abv(s)
+          
+        }else {
         
-                if (freeSeat[i] == true) {
-                    booking.innerHTML = `cadeira ${seat.value} reservada!`
-                }else{
-                    booking.innerHTML = `RESERVADA!`
-                }
-            }
-    
-    
-
-    // newbooking(seat);
-}
-
-function disponiveis(s) {
-    let set = s.value
-    for (i in freeSeat) {
-        if (freeSeat[i] == true) {
-                busySeat.push(set);
-                freeSeat.splice(i, 1);
-                freeSeat[i] = false;
-                return freeSeat[i]
-                
+            booking.innerHTML = `OCUPADO` 
         }
-    }
+
+
+                
 }
 
-// function newbooking(seat) {
-//     let cadeira = seat.value
+
+function ocupar (s){
+   let booking = document.getElementsByClassName("booking")[0];
+   
+    var index = freeSeat.indexOf(s);
+    freeSeat.splice(index,1);
+    console.log(freeSeat);
+
+    busySeat.push(s);
+    console.log(busySeat);
+    booking.innerHTML = `Cadeira ${s} reservada`;
+
+    
+    
+   
+}
+
+
+function abv(marca){
+
     
 
-//     let flag = true;
+        marca.classList.toggle("ocupado");
+        marca.addEventListener("click", abv);
 
-//     while (flag == true) {
-//         let newb = prompt("Deseja fazer uma nova reserva?");
-        
-//         if (newb == "s") {
-//             if (busySeat.indexOf(cadeira) != -1) {
-//                 alert("ocupado")
-                
-//             }else{
-
-//             }
-//         }
-
-        
-        
-//     }
-
-// }
-
-
+    }
+    
     
     
