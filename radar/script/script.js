7717b0f5f0ad28d569df385f86330bba95ad1897
João Elias Maccari <joaomaccari@hotmail.com>
2022-06-13 14:54:26 -0300

    favi

--- radar/script/script.js
+++ radar/script/script.js
@@ -6,8 +6,18 @@ function myFunc() {
     let velo = document.getElementById("input_velo");
 
     if (velo.value <= 80) {
-        msg.innerHTML = "boa viagem"
+        msg.style.backgroundColor = "green"
+        
+        msg.innerHTML = "<p>Limite de velocidade dentro do permitido.<br> Dirija com segurança</p>"
+        velo.value = " ";
+        velo.focus();
     }else{
-        msg.innerHTML = "multado"
+        msg.style.backgroundColor = "lightcoral"
+        let diferenca = velo.value - 80;
+        let multa = diferenca * 7;
+        msg.innerHTML = `<p>MULTADO! Você excedeu o limite permitido que é de 80km/h.</p> 
+        <p>Você deve pagar uma multa no valor de R$${multa.toFixed(2)}!</p>`
+        velo.value = " ";
+        velo.focus();
     }
 }
